4633b06e9eb302351d87c63494893920e2afe096
Agustin <agussamodio9780@gmail.com>
2023-01-13 01:45:56 -0300

    adjunto entrega para railway

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const app = require("./server");
 require("./database");
 
-app.listen(app.get("port"), () => {
+const PORT = process.env.PORT;
+app.listen(PORT || 8080, () => {
   console.log(`server corriendo en el puerto ${app.get("port")} `);
 });
